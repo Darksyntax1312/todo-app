b599284363320845761d31790bc500a30ab040cd
Nexorithm <mayankmadhukar67@gmail.com>
2026-08-08 05:39:25 +0530

    Refactor input handling and created list items

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 let listitems = document.querySelector(".list-items");
 listitems.style.fontSize = "20px";
-// listitems.style.fontFamily = "Inter", sans - serif;
-// listitems.style.fontOpticalSizing = auto;
-// listitems.style.fontweight = 600;
-// listitems.style.fontStyle = normal;
 
 
 let input1 = document.querySelector(".input-box");
 
 let button1 = document.querySelector(".btn-1");
-// button1.style.border = "none";
+
 
 button1.onclick = () => {
     if (input1.value === "") {
@@ -17,17 +13,85 @@ button1.onclick = () => {
 
     }
     else if (input1.value !== null) {
-        // let newelement = document.createElement("li");
-        // newelement.innerText = "•" + " " + input1.value;
-        // listitems.append(newelement)
         createElement()
     }
 
 }
 
+
 function createElement() {
-   let newelement = document.createElement("li");
+    let newdivflx = document.createElement("div");
+    newdivflx.style.display = "flex";
+    newdivflx.style.flexDirection = "row";
+    newdivflx.style.alignItems = "center";
+    newdivflx.style.gap = "10px";
+    listitems.append(newdivflx);
+    let newdiv = document.createElement("div");
+    newdiv.style.border = "2px solid red";
+    newdiv.style.width = "10px";
+    newdiv.style.height = "10px";
+    newdiv.style.borderRadius = "50%";
+    newdivflx.append(newdiv);
+
+    let newelement = document.createElement("li");
+    newelement.style.listStyleType = "none";
     newelement.innerText = input1.value;
-    listitems.append(newelement)
+    newdivflx.append(newelement);
+    let button3 = document.createElement("button");
+    button3.innerText = "➖"
+    newdivflx.append(button3);
+
+    newdiv.onclick = () => {
+        if (
+            newelement.style.textDecorationLine = "line-through") {
+
+        }
+    }
+
+    button3.onclick = () => {
+        newelement.remove();
+        newdivflx.remove()
+        newdiv.remove()
+    }
     input1.value = "";
 }
+
+input1.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        let newdivflx = document.createElement("div");
+        newdivflx.style.display = "flex";
+        newdivflx.style.flexDirection = "row";
+        newdivflx.style.alignItems = "center";
+        newdivflx.style.gap = "10px";
+        listitems.append(newdivflx);
+        let newdiv = document.createElement("div");
+        newdiv.style.border = "2px solid red";
+        newdiv.style.width = "10px";
+        newdiv.style.height = "10px";
+        newdiv.style.borderRadius = "50%";
+        newdivflx.append(newdiv);
+
+ newdiv.onclick = () => {
+        if (
+            newelement.style.textDecorationLine = "line-through") {
+
+        }
+    }
+
+        let newelement = document.createElement("li");
+        newelement.style.listStyleType = "none";
+        newelement.innerText = input1.value;
+        newdivflx.append(newelement);
+        let button3 = document.createElement("button");
+        button3.innerText = "➖"
+        newdivflx.append(button3);
+        button3.onclick = () => {
+            newelement.remove();
+            newdivflx.remove()
+            newdiv.remove()
+        }
+        input1.value = "";
+
+    }
+
+});
